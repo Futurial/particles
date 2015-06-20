5f0b974d5e939cd923b4acc24ddf7f3efa9e009b
futurial <ruskakimov@gmail.com>
2015-06-21 05:22:51 +0800

    objects! and other adventures of Rustem

--- test.js
+++ test.js
@@ -1,65 +1,86 @@
 var c                = document.getElementById("myCanvas"),
     ctx              = c.getContext("2d"),
-    fps              = 50,
+    fps              = 15,
     impactPrcLoss    = 10,
-    cps              = 100, // calculations per second
-    particleSize     = 10;
+    cps              = 20, // calculations per second
+    particleSize     = 2,
+    particleAmount   = 1000;
 
-var particle = {
-    x:        400,
-    y:        400,
-    v:        {
-                x:10,
-                y:11
-              },
-    a:        {
-                x:0,
-                y:0
-              },
+function Particle(x, y, vx, vy) {
+    this.x = x;
+    this.y = y;
+    this.v = {
+        x:vx,
+        y:vy
+    }
+};
 
-    move:        function () {
-                    this.x += this.v.x / cps;
-                    this.y += this.v.y / cps;
+Particle.prototype.a = {
+    x:0,
+    y:100
+};
 
-                    //collision check
-                    var impactCoefficient = (100 - impactPrcLoss) / 100;
-                    if (this.x > c.width - particleSize) {
-                        this.v.x = -this.v.x;
-                        this.x = c.width - particleSize;
-                        this.v.x *= impactCoefficient;
-                    }
-                    if (this.x <= 0) {
-                        this.v.x = -this.v.x;
-                        this.x = 0;
-                        this.v.x *= impactCoefficient;
-                    }
-                    if (this.y > c.height - particleSize) {
-                        this.v.y = -this.v.y;
-                        this.y = c.height - particleSize;
-                        this.v.y *= impactCoefficient;
-                    }
-                    if (this.y <= 0) {
-                        this.v.y = -this.v.y;
-                        this.y = 0;
-                        this.v.y *= impactCoefficient;
-                    }
-                },
-    accelerate:  function () {
-                    this.v.x += this.a.x / cps;
-                    this.v.y += this.a.y / cps;
-                }
+Particle.prototype.move = function () {
+        this.x += this.v.x / cps;
+        this.y += this.v.y / cps;
+
+        //collision check
+        var impactCoefficient = (100 - impactPrcLoss) / 100;
+        if (this.x > c.width - particleSize) {
+            this.v.x = -this.v.x;
+            this.x = c.width - particleSize;
+            this.v.x *= impactCoefficient;
+        }
+        if (this.x <= 0) {
+            this.v.x = -this.v.x;
+            this.x = 0;
+            this.v.x *= impactCoefficient;
+        }
+        if (this.y > c.height - particleSize) {
+            this.v.y = -this.v.y;
+            this.y = c.height - particleSize;
+            this.v.y *= impactCoefficient;
+        }
+        if (this.y <= 0) {
+            this.v.y = -this.v.y;
+            this.y = 0;
+            this.v.y *= impactCoefficient;
+        }
+}
+
+Particle.prototype.accelerate = function () {
+    this.v.x += this.a.x / cps;
+    this.v.y += this.a.y / cps;
+}
+
+var particles = [];
+for (var i = 0; i < particleAmount; i++) {
+    particles[i] = new Particle(
+            rand(0,c.width-particleSize),
+            rand(0,c.width-particleSize),
+            rand(-50,100),
+            rand(-50,100)
+            );
+}
+
+function rand(min, range) {
+    return Math.floor((Math.random() * range) + min);
 }
 
 function draw() {
     ctx.clearRect(0, 0, c.width, c.height);
-    ctx.fillRect(particle.x, particle.y, particleSize, particleSize);
 
+    for (var i = 0; i < particleAmount; i++) {
+        ctx.fillRect(particles[i].x, particles[i].y, particleSize, particleSize);
+    }
     setTimeout(draw, 1000 / fps);
 }
 
 function calculate() {
-    particle.move();
-    particle.accelerate();
+    for (var i = 0; i < particleAmount; i++) {
+        particles[i].move();
+        particles[i].accelerate();
+    }
 
     setTimeout(calculate, 1000 / cps);
 }
