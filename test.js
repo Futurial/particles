bcf31764d451ad9f1e118137e5402211b3310f0a
futurial <ruskakimov@gmail.com>
2015-06-21 04:06:49 +0800

    particle size support added, collision check improved, v and a changed to objects

--- test.js
+++ test.js
@@ -2,49 +2,57 @@ var c                = document.getElementById("myCanvas"),
     ctx              = c.getContext("2d"),
     fps              = 50,
     impactPrcLoss    = 10,
-    cps              = 100 // calculations per second;
+    cps              = 100, // calculations per second
+    particleSize     = 10;
 
 var particle = {
-    x          : 400,
-    y          : 400,
-    v          : [0, 0],
-    a          : [-3, -3],
+    x:        400,
+    y:        400,
+    v:        {
+                x:10,
+                y:11
+              },
+    a:        {
+                x:0,
+                y:0
+              },
 
-    move       : function () {
-                    this.x += this.v[0] / cps;
-                    this.y += this.v[1] / cps;
+    move:        function () {
+                    this.x += this.v.x / cps;
+                    this.y += this.v.y / cps;
 
+                    //collision check
                     var impactCoefficient = (100 - impactPrcLoss) / 100;
-                    if (this.x > c.width) {
-                        this.v[0] = -this.v[0];
-                        this.x = c.width;
-                        this.v[0] *= impactCoefficient;
+                    if (this.x > c.width - particleSize) {
+                        this.v.x = -this.v.x;
+                        this.x = c.width - particleSize;
+                        this.v.x *= impactCoefficient;
                     }
                     if (this.x <= 0) {
-                        this.v[0] = -this.v[0];
+                        this.v.x = -this.v.x;
                         this.x = 0;
-                        this.v[0] *= impactCoefficient;
+                        this.v.x *= impactCoefficient;
                     }
-                    if (this.y > c.height) {
-                        this.v[1] = -this.v[1];
-                        this.y = c.height;
-                        this.v[1] *= impactCoefficient;
+                    if (this.y > c.height - particleSize) {
+                        this.v.y = -this.v.y;
+                        this.y = c.height - particleSize;
+                        this.v.y *= impactCoefficient;
                     }
                     if (this.y <= 0) {
-                        this.v[1] = -this.v[1];
+                        this.v.y = -this.v.y;
                         this.y = 0;
-                        this.v[1] *= impactCoefficient;
+                        this.v.y *= impactCoefficient;
                     }
                 },
-    accelerate : function () {
-                    this.v[0] += this.a[0] / cps;
-                    this.v[1] += this.a[1] / cps;
+    accelerate:  function () {
+                    this.v.x += this.a.x / cps;
+                    this.v.y += this.a.y / cps;
                 }
 }
 
 function draw() {
     ctx.clearRect(0, 0, c.width, c.height);
-    ctx.fillRect(particle.x, particle.y, 5, 5);
+    ctx.fillRect(particle.x, particle.y, particleSize, particleSize);
 
     setTimeout(draw, 1000 / fps);
 }
